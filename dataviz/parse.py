baac97c14a802160c12a2bef14072098869e4958
doublebigmak <mak.michael.609@gmail.com>
2017-01-22 00:24:02 -0800

    Started parse file

---
+++ dataviz/parse.py
@@ -0,0 +1,14 @@
+# dataviz
+# parse.py
+# AUTHOR: Maln
+# TIME: 21/01/2017
+
+import csv
+
+MY_FILE = "../data/sample_sfpd_incident_all.csv"
+
+def parse(raw_file, delimiter):
+    """Parses a raw CSV file to a JSON-line object."""
+
+
+    return parsed_data
