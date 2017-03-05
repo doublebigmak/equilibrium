8f7d204c9561f42194cf5340c61ebd4a07bf3b1b
doublebigmak <mak.michael.609@gmail.com>
2017-03-04 23:10:40 -0800

    alarm started

---
+++ alarm/alarm.py
@@ -0,0 +1,28 @@
+# alarm
+# alarm
+# AUTHOR: Maln
+# TIME: 04/03/2017
+#alarm ??
+import time
+
+usertime = input("Input Time >")
+struct_time = time.strptime(usertime,"%H:%M:%S")
+#calculate difference between input time and current time
+
+user_hour = struct_time[3]
+user_min = struct_time[4]
+
+trigger = 1
+while(trigger):
+    current_time = list(time.localtime())
+    hour = current_time[3]
+    minute = current_time[4]
+    if user_hour == hour and user_min == minute:
+        print("Ding Ding, Alarm triggered!")
+        trigger = 0
+    else:
+        time.sleep(60)
+
+
+
+    #do function
